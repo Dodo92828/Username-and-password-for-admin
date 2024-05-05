c63f77468a834897d74f0b4772a8995d24f9ffb2
Dodo92828 <91278763+Dodo92828@users.noreply.github.com>
2024-05-05 04:17:16 +0300

    Update and rename README.md to TERMINAL.py

---
+++ TERMINAL.py
@@ -0,0 +1,32 @@
+import subprocess
+
+while True:
+
+      
+
+      commands=input("(\033[1;31mROOT\033[1;33m@\033[1;32\033[1;32mHOME\033[1;37m)")
+
+      subprocess.run(commands)
+
+      if commands=="exit":
+
+       print("THE TEMIAL ARE STOPED")
+
+       break
+
+                  
+
+            
+
+     
+
+        
+
+
+
+  
+
+      
+
+
+
