ac390b14f8cc48042c55373507b14014ff79e239
Dodo92828 <91278763+Dodo92828@users.noreply.github.com>
2024-05-05 04:22:24 +0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,10 @@
+
+username="admin"
+password="admin"
+one=input("(\033[1;31mUsername:")
+two=input("(\033[1;31mPassword:")
+if one==username and two==password:
+    print(f'welcom')
+
+else:
+    print("Wrong")
